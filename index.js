5257f84a140fbf9063732d43c63a134acdb32d00
TomTimmel <tom.t.timmel@gmail.com>
2016-10-18 17:31:28 -0700

    lab2 submittal

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-function greet(name) {
-    var greeting = 'hello';
-    var combined = greeting + ' ' + name;
-    return combined;
+const emoji = require('emojic').smiley;
+const readline = require('readline');
+
+module.exports = function greet(){
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('What is your name?', (answer) =>{
+        var greeting = 'Hello';
+        var combined = greeting + ' ' + answer + ' ' + emoji;
+        console.log(combined);
+        rl.close();
+    });
 }
 
-
-module.exports = greet;
+module.exports();
--- test.js
+++ test.js
@@ -1,14 +1,24 @@
-var assert = require('assert');
-var greet = require('./index');
+var assert = require('chai').assert;
+// var greet = require('./index'); 
+// var emoji = require('emojic').smiley;
+var execChild = require('child_process').execSync;
 
-describe ('testing describe', function(){
-    it('greet with name', function(){
-        var greeting = greet('tester');
-
-        assert.equal(greeting, 'hello tester');
-    });
-    it('no param in function', function(){
-        var greetingEmpty = greet();
-        assert.equal(greetingEmpty, 'hello undefined');
-    });
+it('greet with name', function(){
+    var regex = /^What is your name/g;
+    var greeting = execChild('node index.js', {encoding: 'utf-8'});
+    assert.match(greeting, regex);
 });
+
+
+
+// describe ('testing describe', function(){
+//     it('greet with name', function(){
+//         var greeting = greet('tester');
+
+//         assert.equal(greeting, 'Hello tester' + ' ' + emoji);
+//     });
+//     it('no param in function', function(){
+//         var greetingEmpty = greet();
+//         assert.equal(greetingEmpty, 'Hello undefined' + ' ' + emoji);
+//     });
+// });
